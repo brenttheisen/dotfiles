fb4d3f12f1363940d5066c8bab7fbdbb71f3b28f
Brent Theisen <brent@bantamlabs.com>
2026-05-10 10:23:30 -0500

    Better Neovim startup screen

--- nvim/init.lua
+++ nvim/init.lua
@@ -27,6 +27,37 @@ require("lazy").setup({
     vim.cmd.colorscheme("solarized")
   end },
 
+  -- Startup screen
+  {
+    "goolord/alpha-nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      local alpha = require("alpha")
+      local dashboard = require("alpha.themes.dashboard")
+
+      dashboard.section.header.val = {
+        "                                                     ",
+        "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗",
+        "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║",
+        "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║",
+        "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║",
+        "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║",
+        "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝",
+        "                                                     ",
+      }
+
+      dashboard.section.buttons.val = {
+        dashboard.button("e", "  New file",        ":ene <BAR> startinsert <CR>"),
+        dashboard.button("f", "  Find file",       ":Telescope find_files<CR>"),
+        dashboard.button("r", "  Recent files",    ":Telescope oldfiles<CR>"),
+        dashboard.button("g", "  Find text",       ":Telescope live_grep<CR>"),
+        dashboard.button("q", "  Quit",            ":qa<CR>"),
+      }
+
+      alpha.setup(dashboard.config)
+    end,
+  },
+
   -- UI
   { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" }, config = true },
   { "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons", config = true },
