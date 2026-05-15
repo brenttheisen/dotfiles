ae06fb859cb29c47e0657f1e44c1d1a095b7d9dc
Brent Theisen <brent@bantamlabs.com>
2026-05-14 23:22:28 -0500

    Better tmux right status line segments

--- tmux-powerline/themes/wasted.sh
+++ tmux-powerline/themes/wasted.sh
@@ -2,15 +2,15 @@
 # If changes made here does not take effect, then try to re-create the tmux session to force reload.
 
 if patched_font_in_use; then
-	TMUX_POWERLINE_SEPARATOR_LEFT_BOLD=""
-	TMUX_POWERLINE_SEPARATOR_LEFT_THIN=""
-	TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD=""
-	TMUX_POWERLINE_SEPARATOR_RIGHT_THIN=""
+  TMUX_POWERLINE_SEPARATOR_LEFT_BOLD=""
+  TMUX_POWERLINE_SEPARATOR_LEFT_THIN=""
+  TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD=""
+  TMUX_POWERLINE_SEPARATOR_RIGHT_THIN=""
 else
-	TMUX_POWERLINE_SEPARATOR_LEFT_BOLD="◀"
-	TMUX_POWERLINE_SEPARATOR_LEFT_THIN="❮"
-	TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD="▶"
-	TMUX_POWERLINE_SEPARATOR_RIGHT_THIN="❯"
+  TMUX_POWERLINE_SEPARATOR_LEFT_BOLD="◀"
+  TMUX_POWERLINE_SEPARATOR_LEFT_THIN="❮"
+  TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD="▶"
+  TMUX_POWERLINE_SEPARATOR_RIGHT_THIN="❯"
 fi
 
 TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR=${TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR:-'235'}
@@ -24,30 +24,30 @@ TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SE
 # The `format regular` and `format inverse` functions are provided as conveniences
 
 if [ -z $TMUX_POWERLINE_WINDOW_STATUS_CURRENT ]; then
-	TMUX_POWERLINE_WINDOW_STATUS_CURRENT=(
-		"#[$(format inverse)]" \
-		"$TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR" \
-		" #I#F " \
-		"$TMUX_POWERLINE_SEPARATOR_RIGHT_THIN" \
-		" #W " \
-		"#[$(format regular)]" \
-		"$TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR"
-	)
+  TMUX_POWERLINE_WINDOW_STATUS_CURRENT=(
+    "#[$(format inverse)]"
+    "$TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR"
+    " #I#F "
+    "$TMUX_POWERLINE_SEPARATOR_RIGHT_THIN"
+    " #W "
+    "#[$(format regular)]"
+    "$TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR"
+  )
 fi
 
 if [ -z $TMUX_POWERLINE_WINDOW_STATUS_STYLE ]; then
-	TMUX_POWERLINE_WINDOW_STATUS_STYLE=(
-		"$(format regular)"
-	)
+  TMUX_POWERLINE_WINDOW_STATUS_STYLE=(
+    "$(format regular)"
+  )
 fi
 
 if [ -z $TMUX_POWERLINE_WINDOW_STATUS_FORMAT ]; then
-	TMUX_POWERLINE_WINDOW_STATUS_FORMAT=(
-		"#[$(format regular)]" \
-		"  #I#{?window_flags,#F, } " \
-		"$TMUX_POWERLINE_SEPARATOR_RIGHT_THIN" \
-		" #W "
-	)
+  TMUX_POWERLINE_WINDOW_STATUS_FORMAT=(
+    "#[$(format regular)]"
+    "  #I#{?window_flags,#F, } "
+    "$TMUX_POWERLINE_SEPARATOR_RIGHT_THIN"
+    " #W "
+  )
 fi
 
 # Format: segment_name background_color foreground_color [non_default_separator] [separator_background_color] [separator_foreground_color] [spacing_disable] [separator_disable]
@@ -78,38 +78,38 @@ fi
 # of options to support the spacing_disable and separator_disable features can be used
 
 if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
-	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
-		"tmux_session_info 004 255" \
-		#"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
-		#"vcs_branch 10 20" \
-		#"vcs_compare 60 255" \
-		#"vcs_staged 64 255" \
-		#"vcs_modified 9 255" \
-		#"vcs_others 245 0" \
-	)
+  TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
+    "tmux_session_info 004 255"
+    #"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
+    #"vcs_branch 10 20" \
+    #"vcs_compare 60 255" \
+    #"vcs_staged 64 255" \
+    #"vcs_modified 9 255" \
+    #"vcs_others 245 0" \
+  )
 fi
 
 if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
-	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
-		#"earthquake 3 0" \
-		# "pwd 89 211" \
-		#"macos_notification_count 29 255" \
-		#"mailcount 9 255" \
-		"now_playing 234 37" \
-		#"cpu 240 136" \
-		#"ifstat 30 255" \
-		"wan_ip 24 255" \
-		"ifstat_sys 30 255" \
-		"load 237 167" \
-		#"tmux_mem_cpu_load 234 136" \
-		"battery 0" \
+  TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
+    "time 4"
+    #"weather 37 255"
+    "battery 0"
+    #"earthquake 3 0" \
+    # "pwd 89 211" \
+    #"macos_notification_count 29 255" \
+    #"mailcount 9 255" \
+    #"cpu 240 136"
+    "tmux_mem_cpu_load 234 136"
+    "ifstat 30 255"
+    "wan_ip 24 255"
+    "ifstat_sys 30 255"
+    "load 237 167"
+    "now_playing 234 37"
     #"air ${TMUX_POWERLINE_SEG_AIR_COLOR} 255" \
-		# "weather 37 255" \
-		#"rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}" \
-		#"xkb_layout 125 117" \
-		"date_day 235 136" \
-		#"date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-		"time 4" \
-		#"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-	)
+    #"rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}" \
+    #"xkb_layout 125 117" \
+    #"date_day 235 136" \
+    #"date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+    #"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+  )
 fi
