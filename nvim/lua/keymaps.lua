c479bc8be7d649dd9246298671d635f844fd0322
Brent Theisen <brent@bantamlabs.com>
2026-06-01 21:59:29 -0500

    Added some git related leaders

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -21,8 +21,17 @@ keymap("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Find string 
 keymap("n", "<leader>fw", "<cmd>Telescope grep_string<cr>", { desc = "Find word under cursor" })
 keymap("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "List open buffers" })
 keymap("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "List help tags" })
+keymap("n", "<leader>gc", "<cmd>Telescope git_commits<cr>", { desc = "Git commits (Telescope)" })
+
+-- Git
+keymap("n", "<leader>gl", "<cmd>Git log --graph --oneline --decorate --all<cr>", { desc = "Git log graph (Fugitive)" })
 
 -- Trouble
 keymap("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", { desc = "Diagnostics (Trouble)" })
 keymap("n", "<leader>xw", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", { desc = "Buffer Diagnostics (Trouble)" })
 keymap("n", "gR", "<cmd>Trouble lsp_references toggle<cr>", { desc = "LSP References (Trouble)" })
+
+-- Terminal
+keymap("n", "<leader>t", ":botright 15split | term<CR>", { desc = "Open terminal in bottom split" })
+keymap("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
+
